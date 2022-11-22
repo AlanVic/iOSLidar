913af14def2d8ceaf43dc7d267a238b367b6dafb
Alan Paulino <alanvictor5447@gmail.com>
2022-11-22 12:44:08 -0300

    including DS_STore on gitignore and modify the project

--- TestingiOSLidar/ExperienceMeasureViewController.swift
+++ TestingiOSLidar/ExperienceMeasureViewController.swift
@@ -6,4 +6,48 @@
 //
 
 import Foundation
+import ARKit
+import RealityKit
 
+class ExperienceMeasureViewController: UIViewController, ARSessionDelegate {
+    @IBOutlet weak var augmentedView: ARView!
+    
+    @IBAction func didTapView(_ sender: UITapGestureRecognizer) {
+        print("did tap view", sender)
+        let location = sender.location(in: augmentedView)
+        print("the location is: ", location)
+        print("the transformation is: " , convertCGPointToLidarRelation(location: location))
+        addACircleToView(onLocation: location)
+    }
+    
+    func setupARConfiguration() -> ARConfiguration {
+        let configuration = ARWorldTrackingConfiguration()
+        
+        
+        if ARWorldTrackingConfiguration.supportsFrameSemantics(.sceneDepth) {
+            configuration.frameSemantics = .sceneDepth
+        }
+        
+        return configuration
+    }
+    
+    func convertCGPointToLidarRelation(location: CGPoint) -> CGPoint {
+        let x = location.x * 192 / augmentedView.frame.width
+        let y = location.y * 256 / augmentedView.frame.height
+        return CGPoint(x: x, y: y)
+    }
+    
+    func addACircleToView(onLocation location: CGPoint) {
+//        let circleLayer = CAShapeLayer()
+//        circleLayer.path = UIBezierPath(ovalIn: CGRect(x: location.x, y: location.y, width: 10, height: 10)).cgPath
+        
+        let cicleView = UIView(frame: CGRect(x: location.x, y: location.y, width: 10, height: 10))
+        cicleView.layer.cornerRadius = cicleView.frame.width / 2
+        cicleView.backgroundColor = .red
+        
+//        augmentedView.subviews.forEach {
+//            $0.removeFromSuperview()
+//        }
+        augmentedView.addSubview(cicleView)
+    }
+}
